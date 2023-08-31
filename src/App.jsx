6ba195c0dd1980d92c13498675d372a8813f57e0
Alexander Hergert <alexander.hergert1989@gmail.com>
2023-08-31 21:43:32 +0200

    First commit. Template setup.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <main>
+      <h1 className="text-center font-bold red">App template</h1>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/utility.js
@@ -0,0 +1,24 @@
+// Function to save data to localStorage
+export function saveDataToLocalStorage(key, data) {
+  try {
+    const serializedData = JSON.stringify(data);
+    localStorage.setItem(key, serializedData);
+  } catch (error) {
+    console.error(`Error saving data to localStorage: ${error}`);
+  }
+}
+
+// Function to load data from localStorage
+export function loadDataFromLocalStorage(key) {
+  try {
+    const serializedData = localStorage.getItem(key);
+    if (serializedData === null) {
+      return null;
+    }
+    const data = JSON.parse(serializedData);
+    return data;
+  } catch (error) {
+    console.error(`Error loading data from localStorage: ${error}`);
+    return null;
+  }
+}
